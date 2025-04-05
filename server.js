414c1b1761db26f2ec94f21b15066eee5e387666
Joe Tito <joetito1@gmail.com>
2025-04-05 14:26:57 -0400

    Updates

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const SECRET_KEY = "supersecretkey123";
+// const SECRET_KEY = "supersecretkey123";
+
+// app.get('/', (req, res) => {
+//     res.send('Node.js App Running!');
+// });
+
+// app.get('/debug', (req, res) => {
+//     res.json({
+//         message: "Debug Info",
+//         secret: SECRET_KEY,
+//         env: process.env
+//     });
+// });
+
+// const PORT = process.env.PORT || 80;
+// app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`App running on http://0.0.0.0:${PORT}`);
+// }); 
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('Node.js App Running!');
-});
+  res.send('Hello World!')
+})
 
-app.get('/debug', (req, res) => {
-    res.json({
-        message: "Debug Info",
-        secret: SECRET_KEY,
-        env: process.env
-    });
-});
-
-const PORT = process.env.PORT || 80;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`App running on http://0.0.0.0:${PORT}`);
-}); 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
